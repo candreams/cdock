f8916126382e50a9e6d632958869b1639e4b935b
Emy <me@emy.moe>
2018-10-20 08:19:38 +0200

    initial commit

---
+++ cdockpreferences/cdpRootListController.m
@@ -0,0 +1,13 @@
+#include "cdpRootListController.h"
+
+@implementation cdpRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [[self loadSpecifiersFromPlistName:@"Root" target:self] retain];
+	}
+
+	return _specifiers;
+}
+
+@end
